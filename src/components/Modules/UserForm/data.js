ee8ac0a434c28a3c730a9c9869016253efe967c2
manas gupta <manasgupta0110@gmail.com>
2023-04-19 00:49:38 +0530

    finishing fixes

--- src/components/Modules/UserForm/data.js
+++ src/components/Modules/UserForm/data.js
@@ -1,13 +1,56 @@
 import { randomColor } from "../../utils/constants";
 
 const staticArrayData = [
-  { name: "manas", email: "manas@m", phoneNumber: "9988998899" },
-  { name: "manas2", email: "manas@m", phoneNumber: "9988998899" },
-  { name: "manas3", email: "manas@m", phoneNumber: "9988998899" },
-  { name: "manas4", email: "manas@m", phoneNumber: "9988998899" },
-  { name: "manas5", email: "manas@m", phoneNumber: "9988998899" },
-  { name: "manas6", email: "manas@m", phoneNumber: "9988998899" },
-  { name: "manas7", email: "manas@m", phoneNumber: "9988998899" },
+  {
+    name: "John Doe",
+    email: "johndoe@example.com",
+    phone: "555-555-5555",
+  },
+  {
+    name: "Jane Smith",
+    email: "janesmith@example.com",
+    phone: "555-555-5555",
+  },
+  {
+    name: "Bob Johnson",
+    email: "bobjohnson@example.com",
+    phone: "555-555-5555",
+  },
+  {
+    name: "Emily Chen",
+    email: "emilychen@example.com",
+    phone: "555-555-5555",
+  },
+  {
+    name: "Mike Garcia",
+    email: "mikegarcia@example.com",
+    phone: "555-555-5555",
+  },
+  {
+    name: "Samantha Lee",
+    email: "samanthalee@example.com",
+    phone: "555-555-5555",
+  },
+  {
+    name: "David Nguyen",
+    email: "davidnguyen@example.com",
+    phone: "555-555-5555",
+  },
+  {
+    name: "Karen Taylor",
+    email: "karentaylor@example.com",
+    phone: "555-555-5555",
+  },
+  {
+    name: "Kevin Wong",
+    email: "kevinwong@example.com",
+    phone: "555-555-5555",
+  },
+  {
+    name: "Rachel Kim",
+    email: "rachelkim@example.com",
+    phone: "555-555-5555",
+  },
 ];
 const getUsersWithColor = (arr) => {
   return arr.map((item) => ({ ...item, color: randomColor() }));
--- src/components/UIElements/Input/index.js
+++ src/components/UIElements/Input/index.js
@@ -12,29 +12,35 @@ export default function Input({
   return (
     <div className="inputContainer" style={{ marginBottom: "8px" }}>
       <label className="label">{label}</label>
-      <input
-        className="input"
-        type={type}
-        value={value}
-        onChange={(e) =>
-          onChange(type == "file" ? e.target.files[0] : e.target.value)
-        }
-        placeholder={placeholder}
-      />
-
-      {errorMessage && value != "" ? (
-        <p
+      <div className="wrapper">
+        <input
+          className="input"
+          type={type}
+          value={value}
+          onChange={(e) =>
+            onChange(type == "file" ? e.target.files[0] : e.target.value)
+          }
+          placeholder={placeholder}
           style={{
-            color: "rgb(217,17,0)",
-            fontSize: "12px",
-            fontWeight: "bold",
-            margin: "0",
-            padding: "0",
+            backgroundColor: type == "file" ? "#505050" : undefined,
           }}
-        >
-          {errorMessage}
-        </p>
-      ) : null}
+        />
+
+        {errorMessage && value != "" ? (
+          <p
+            style={{
+              color: "rgb(217,17,0)",
+              fontSize: "12px",
+              fontWeight: "bold",
+              margin: "0",
+              padding: "0",
+              fontWeight: "bold",
+            }}
+          >
+            {errorMessage}
+          </p>
+        ) : null}
+      </div>
     </div>
   );
 }
